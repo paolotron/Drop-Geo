d7adaaf7017673178f84b015762234378d499a9e
Alkio84 <79812915+Alkio84@users.noreply.github.com>
2022-01-20 17:33:33 +0100

    feat: added pitts in st_lucia_test.py

--- source/st_lucia_test.py
+++ source/st_lucia_test.py
@@ -9,11 +9,19 @@ import multiprocessing
 import network
 import test
 
+"""
+SCELTA DATASET:
+"pitts30k"
+"st_lucia"
+"""
+ds = "pitts30k"
+
 if __name__ == '__main__':
+
     args = myparser.parse_arguments()
 
     start_time = datetime.now()
-    args.output_folder = join("runs/st_lucia", args.exp_name, start_time.strftime('%Y-%m-%d_%H-%M-%S'))
+    args.output_folder = join("runs", ds, args.exp_name, start_time.strftime('%Y-%m-%d_%H-%M-%S'))
     best_model_path = args.best_model_path
     commons.setup_logging(args.output_folder)
     commons.make_deterministic(args.seed)
@@ -22,9 +30,9 @@ if __name__ == '__main__':
     logging.info(f"Using {torch.cuda.device_count()} GPUs and {multiprocessing.cpu_count()} CPUs")
 
     # Creation of Datasets
-    logging.debug(f"Loading dataset st_lucia from folder {args.datasets_folder}")
+    logging.debug(f"Loading dataset {ds} from folder {args.datasets_folder}")
 
-    test_ds = datasets_ws.BaseDataset(args, args.datasets_folder, "st_lucia", "test")
+    test_ds = datasets_ws.BaseDataset(args, args.datasets_folder, ds, "test")
     logging.info(f"Test set: {test_ds}")
 
     model = network.GeoLocalizationNet(args)
@@ -37,3 +45,5 @@ if __name__ == '__main__':
 
     recalls, recalls_str = test.test(args, test_ds, model)
     logging.info(f"Recalls on {test_ds}: {recalls_str}")
+
+
